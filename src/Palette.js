f7fc9159cc2898ffdc231b0edafcf8c957f2d33d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-18 13:17:44 -0500

    added Palette

---
+++ src/Palette.js
@@ -0,0 +1,7 @@
+const Primary = "#D9A9B6";
+const Secondary = "#87A617";
+const AltPrimary = "#593A1E";
+const AltSecondary = "#798C35";
+const Text = "#0D1B2A";
+const Alert = "#963939";
+export { Primary, Secondary, AltPrimary, AltSecondary, Text, Alert };
